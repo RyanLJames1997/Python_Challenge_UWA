dfe24e6d3e99d993921911e4348175b9eaf47d69
RyanJames1997 <R.James1997_@outlook.com>
2023-08-10 18:08:21 +0800

    add

---
+++ PyPoll/Resources/PyPoll_Analysis.py
@@ -0,0 +1,7 @@
+import os
+import csv
+
+csvpath = os.path.join('.', 'Resources', 'election_data.csv')
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)
